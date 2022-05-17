25b6987dc19a757df6f3253b42af7444080544ab
ansumariamjacob <ansumariam99@gmail.com>
2022-05-17 14:56:25 +0530

    view book

--- src/component/Viewbook.js
+++ src/component/Viewbook.js
@@ -15,15 +15,15 @@ const Viewbook = () => {
                 <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
                     <div className='row g-3'>
                         <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-                            <label className='form-label'>Title:-{booklist.title}</label>
+                            <label className='form-label'>Title:- {booklist.title}</label>
 
                         </div>
                         <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-                            <label className='form-label'>Author:-{booklist.author}</label>
+                            <label className='form-label'>Author:- {booklist.author}</label>
 
                         </div>
                         <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-                            <label className='form-label'>Price:-{booklist.price}</label>
+                            <label className='form-label'>Price:- {booklist.price}</label>
 
                         </div>
                     </div>
