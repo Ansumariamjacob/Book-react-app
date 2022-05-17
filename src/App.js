a42115a54d0d7fbea9e82387bf1105622ad8411d
ansumariamjacob <ansumariam99@gmail.com>
2022-05-17 12:19:15 +0530

    searchbook

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Addbook from './component/Addbook';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Booksearch from './component/Booksearch';
 function App() {
   return (
     <div >
       <Addbook/>
+      <Booksearch/>
     </div>
   );
 }
